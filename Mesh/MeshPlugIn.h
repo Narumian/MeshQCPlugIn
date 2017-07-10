1df03074dcfad4d250603d269655c87fe2c04bbd
narumiand <joujitsu@narumi-no-macbook-pro-4.local>
2017-07-11 04:10:38 +0900

    Initial Commit

---
+++ Mesh/MeshPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  MeshPlugIn.h
+//  Mesh
+//
+//  Created by Narumi on 2017/07/11.
+//  Copyright © 2017年 Narumi Inada. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface MeshPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
