31fd9f46bf18f3b8221429cc029ace5d0cbb12a2
Narumian <1056shigezane@gmail.com>
2018-04-18 10:27:17 +0900

    First Commit

--- Mesh/MeshPlugIn.h
+++ Mesh/MeshPlugIn.h
@@ -3,12 +3,36 @@
 //  Mesh
 //
 //  Created by Narumi on 2017/07/11.
-//  Copyright © 2017年 Narumi Inada. All rights reserved.
+//  2017年 Narumi Inada.Use for free.
 //
 
 #import <Quartz/Quartz.h>
+#import <GLKit/GLKit.h>
 
 @interface MeshPlugIn : QCPlugIn
+{
+    
+}
+
+@property (assign) double inputX;
+@property (assign) double inputY;
+@property (assign) double inputZ;
+@property (assign) double inputRotationX;
+@property (assign) double inputRotationY;
+@property (assign) double inputRotationZ;
+@property (assign) double inputScale;
+
+@property (assign)NSArray* inputVertices;
+@property (assign)NSArray* inputColors;
+@property (assign)NSArray* inputNormals;
+@property (assign)NSArray* inputTexCoords;
+@property (assign) NSUInteger inputMode;
+@property (assign) NSUInteger inputBlendMode;
+@property (assign) NSUInteger inputCullMode;
+@property (assign) double inputsize;
+@property (assign) id<QCPlugInInputImageSource> inputTexture;
+
+//@property (assign) NSString* outputDebug;
 
 // Declare here the properties to be used as input and output ports for the plug-in e.g.
 //@property double inputFoo;
--- Mesh/MeshPlugIn.m
+++ Mesh/MeshPlugIn.m
@@ -17,7 +17,7 @@
 @implementation MeshPlugIn
 
 // Here you need to declare the input / output properties as dynamic as Quartz Composer will handle their implementation
-//@dynamic inputFoo, outputBar;
+@dynamic inputVertices, inputColors, inputNormals, inputTexCoords, inputMode,inputBlendMode,inputCullMode ,inputsize ,inputTexture,inputX,inputY,inputZ,inputRotationX,inputRotationY,inputRotationZ,inputScale;
 
 + (NSDictionary *)attributes
 {
@@ -28,13 +28,46 @@
 + (NSDictionary *)attributesForPropertyPortWithKey:(NSString *)key
 {
     // Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
+    if([key isEqualToString:@"inputMode"])
+        return  @{QCPortAttributeNameKey : @"Mode",
+                  QCPortAttributeMenuItemsKey : @[@"GL_POINTS",@"GL_LINES",@"GL_LINE_LOOP",@"GL_LINE_STRIP",@"GL_TRIANGLES",@"GL_TRIANGLE_STRIP",@"GL_TRIANGLE_FAN",@"GL_QUADS",@"GL_QUAD_STRIP",@"GL_POLYGON",@"POINTSPRITE"],
+                  QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
+                  QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:0],
+                  QCPortAttributeMaximumValueKey : [NSNumber numberWithUnsignedInteger:10]};
+    
+    if([key isEqualToString:@"inputBlendMode"])
+        return  @{QCPortAttributeNameKey : @"BlendMode",
+                  QCPortAttributeMenuItemsKey : @[@"REPLACE",@"OVER",@"ADD"],
+                  QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
+                  QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:0],
+                  QCPortAttributeMaximumValueKey : [NSNumber numberWithUnsignedInteger:2]};
+    if([key isEqualToString:@"inputCullMode"])
+        return  @{QCPortAttributeNameKey : @"CullingMode",
+                  QCPortAttributeMenuItemsKey : @[@"None",@"FrontFaces",@"BackFaces",@"Front&Back Faces"],
+                  QCPortAttributeMinimumValueKey : [NSNumber numberWithUnsignedInteger:0],
+                  QCPortAttributeDefaultValueKey : [NSNumber numberWithUnsignedInteger:0],
+                  QCPortAttributeMaximumValueKey : [NSNumber numberWithUnsignedInteger:3]};
+    
+    /*
+     GL_POINTS                         0x0000
+     #define GL_LINES                          0x0001
+     #define GL_LINE_LOOP                      0x0002
+     #define GL_LINE_STRIP                     0x0003
+     #define GL_TRIANGLES                      0x0004
+     #define GL_TRIANGLE_STRIP                 0x0005
+     #define GL_TRIANGLE_FAN                   0x0006
+     #define GL_QUADS                          0x0007
+     #define GL_QUAD_STRIP                     0x0008
+     #define GL_POLYGON
+     */
+    
     return nil;
 }
 
 + (QCPlugInExecutionMode)executionMode
 {
     // Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
-    return kQCPlugInExecutionModeProcessor;
+    return kQCPlugInExecutionModeConsumer;
 }
 
 + (QCPlugInTimeMode)timeMode
@@ -48,6 +81,9 @@
     self = [super init];
     if (self) {
         // Allocate any permanent resource required by the plug-in.
+        //self.outputDebug = @"AAA";
+        
+        
     }
     
     return self;
@@ -73,14 +109,249 @@
 
 - (BOOL)execute:(id <QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary *)arguments
 {
-    /*
-    Called by Quartz Composer whenever the plug-in instance needs to execute.
-    Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
-    Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
-    
-    The OpenGL context for rendering can be accessed and defined for CGL macros using:
     CGLContextObj cgl_ctx = [context CGLContextObj];
-    */
+    //GL_TRIANGLES
+    GLuint mode;
+    //GL_LINES
+    
+    
+    
+    switch(self.inputMode){
+        case 0:mode = 0x0000;break;
+        case 1:mode = 0x0001;break;
+        case 2:mode = 0x0002;break;
+        case 3:mode = 0x0003;break;
+        case 4:mode = 0x0004;break;
+        case 5:mode = 0x0005;break;
+        case 6:mode = 0x0006;break;
+        case 7:mode = 0x0007;break;
+        case 8:mode = 0x0008;break;
+        case 9:mode = 0x0009;break;
+        case 10:mode = 0x0000;break;
+        default:mode = 0x0000;break;
+    }
+    
+    NSInteger colorSize,normSize,coordSize;
+    
+    colorSize = [self.inputColors count];
+    normSize = [self.inputNormals count];
+    coordSize = [self.inputTexCoords count];
+    
+    BOOL safe = YES;
+    BOOL alpha = NO;
+    BOOL withw = NO;
+    BOOL withwN = NO;
+    if([self.inputVertices count] > 0 && [[self.inputVertices objectAtIndex:0] isKindOfClass:[NSArray class]]){
+        if([[self.inputVertices objectAtIndex:0] count] == 3){
+            safe = YES;
+        }else if([[self.inputVertices objectAtIndex:0] count] == 4){
+            withw = YES;
+            safe = YES;
+        }else{
+            safe = NO;
+            
+        }
+    }else{
+        safe = NO;
+        
+    }
+    
+    if(colorSize>0){
+        if([[self.inputColors objectAtIndex:0] isKindOfClass:[NSArray class]]){
+            if([[self.inputColors objectAtIndex:0] count] == 3){
+                safe = YES;
+            }else if([[self.inputColors objectAtIndex:0] count] == 4){
+                safe = YES;
+                alpha = YES;
+            }else{
+                safe = NO;
+            }
+        }else{
+            safe = NO;
+        }
+    }
+    
+    
+    if(coordSize>0){
+        if([[self.inputTexCoords objectAtIndex:0] isKindOfClass:[NSArray class]]){
+            if([[self.inputTexCoords objectAtIndex:0] count] == 2){
+                safe = YES;
+            }else{
+                safe = NO;
+            }
+        }else{
+            safe = NO;
+        }
+    }
+    
+    if(normSize>0){
+        if([[self.inputNormals objectAtIndex:0] isKindOfClass:[NSArray class]]){
+            if([[self.inputNormals objectAtIndex:0] count] == 3){
+                safe = YES;
+            }else if([[self.inputNormals objectAtIndex:0] count] == 4){
+                safe = YES;
+                withwN = YES;
+            }else{
+                safe = NO;
+            }
+        }else{
+            safe = NO;
+        }
+     }
+     
+    
+    
+    if(safe){
+        //glDepthMask(GL_FALSE);
+        glEnable(GL_BLEND);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_NORMALIZE);
+        glActiveTexture(GL_TEXTURE0);
+        //glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+        /*if(alpha){
+            
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            
+        }else{
+            
+            
+            
+        }*/
+        
+        switch(self.inputBlendMode){
+            case 0:glBlendFunc(GL_ONE, GL_ZERO);break;
+            case 1:glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);break;
+            case 2:glBlendFunc(GL_ONE, GL_ONE);break;
+            default:break;
+                
+        }
+        
+        if(self.inputCullMode!=0){
+            //glFrontFace(GL_CCW);
+            glEnable(GL_CULL_FACE);
+        }
+        switch(self.inputCullMode){
+            case 0:break;
+            case 1:glCullFace(GL_FRONT);
+            case 2:glCullFace(GL_BACK);
+            case 3:glCullFace(GL_FRONT_AND_BACK);
+            default:break;
+        }
+        
+        
+        
+        id<QCPlugInInputImageSource>    image;
+        GLuint textureName;
+        GLint saveMode;
+        
+        
+        image = self.inputTexture;
+        
+        if(image && [image lockTextureRepresentationWithColorSpace:([image shouldColorMatch] ? [context colorSpace] : [image imageColorSpace] ) forBounds:[image imageBounds]])textureName = [image textureName];
+        else textureName = 0;
+        
+        glGetIntegerv(GL_MATRIX_MODE, &saveMode);
+        glMatrixMode(GL_MODELVIEW);
+        if(textureName){
+            glEnable([image textureTarget]);
+            [image bindTextureRepresentationToCGLContext:cgl_ctx textureUnit:GL_TEXTURE0 normalizeCoordinates:YES];
+            
+            
+        }
+        
+        
+        
+        
+        if(mode==GL_POINTS){
+            
+            if(self.inputMode == 10){//point sprite
+                glActiveTexture(GL_TEXTURE0);
+                glEnable(GL_POINT_SPRITE);
+                if([image textureTarget] == GL_TEXTURE_2D)glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+                glTexEnvf(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE);
+
+            }
+            
+            glPointSize(self.inputsize);
+        }else{
+            if(self.inputMode == 1||self.inputMode == 2||self.inputMode == 3){
+            glLineWidth(self.inputsize);
+            }
+        }
+        
+        glPushMatrix();
+        glScaled(self.inputScale, self.inputScale, self.inputScale);
+        glTranslated(self.inputX, self.inputY, self.inputZ);
+        glRotated(self.inputRotationX, 1.0, 0.0, 0.0);
+        glRotated(self.inputRotationY, 0.0, 1.0, 0.0);
+        glRotated(self.inputRotationZ, 0.0, 0.0, 1.0);
+        
+        
+        glBegin(mode);
+        
+        
+        
+        for(NSInteger i = 0; i < [self.inputVertices count]; i++){
+            if(i<colorSize){
+                if(alpha){
+                    glColor4f([[self.inputColors objectAtIndex:i][0] floatValue], [[self.inputColors objectAtIndex:i][1] floatValue], [[self.inputColors objectAtIndex:i][2] floatValue],[[self.inputColors objectAtIndex:i][3] floatValue]);
+                }
+                else{
+                    glColor3f([[self.inputColors objectAtIndex:i][0] floatValue], [[self.inputColors objectAtIndex:i][1] floatValue], [[self.inputColors objectAtIndex:i][2] floatValue]);
+                }
+            }
+            
+            
+            if(i<normSize){
+                
+               
+                    glNormal3f([[self.inputNormals objectAtIndex:i][0] floatValue], [[self.inputNormals objectAtIndex:i][1] floatValue], [[self.inputNormals objectAtIndex:i][2] floatValue]);
+                
+            }
+            
+            if(i<coordSize)glTexCoord2f([self.inputTexCoords[i][0] floatValue], [self.inputTexCoords[i][1] floatValue]);
+            
+            if(withw){
+                glVertex4f([[self.inputVertices objectAtIndex:i][0] floatValue], [[self.inputVertices objectAtIndex:i][1] floatValue], [[self.inputVertices objectAtIndex:i][2] floatValue],[[self.inputVertices objectAtIndex:i][3] floatValue]);
+            }else{
+                glVertex3f([[self.inputVertices objectAtIndex:i][0] floatValue], [[self.inputVertices objectAtIndex:i][1] floatValue], [[self.inputVertices objectAtIndex:i][2] floatValue]);
+            }
+        }
+        
+        
+        glColor3f(1.0,1.0,1.0);
+        glEnd();
+        
+        glPopMatrix();
+        glDisable(GL_BLEND);
+        glDisable(GL_CULL_FACE);
+        //glBindTexture(tgt,0);
+        //glDisable(tgt);
+        glMatrixMode(saveMode);
+       
+        
+        if(mode==GL_POINT){
+            
+            if(self.inputMode == 10){//point sprite
+                glDisable(GL_POINT_SPRITE);
+                glTexEnvf(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_FALSE);
+                
+            }
+            glPointSize(0);
+        }else{
+            if(self.inputMode == 1||self.inputMode == 2||self.inputMode == 3)glLineWidth(0);
+        }
+        if(textureName){
+            [image unbindTextureRepresentationFromCGLContext:cgl_ctx textureUnit:GL_TEXTURE0];
+            [image unlockTextureRepresentation];
+            
+        }
+        
+        
+        
+    }
+    
+    //glvertexpoi
     
     return YES;
 }
